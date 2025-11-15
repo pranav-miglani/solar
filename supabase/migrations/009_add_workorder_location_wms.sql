5f53d6f418c5ccefa00425debf3ce8df4a6970d8
Apple <apple@Apples-MacBook-Pro.local>
2025-11-16 02:21:17 +0530

    update

--- supabase/migrations/009_add_workorder_location_wms.sql
+++
@@ -1,9 +0,0 @@
--- Add location field to work_orders table
-ALTER TABLE work_orders 
-ADD COLUMN IF NOT EXISTS location TEXT;
-
--- Add index for location if needed for searches
-CREATE INDEX IF NOT EXISTS idx_work_orders_location ON work_orders(location);
-
-COMMENT ON COLUMN work_orders.location IS 'Physical location of the work order';
-
